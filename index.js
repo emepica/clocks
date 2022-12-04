08b1facc96e6759b1e136b4c0dee38f8256d2cd0
emepica <emeline.picard@hotmail.com>
2022-12-04 15:30:24 +0100

    Added working functionality for drop down selected option
    Added js code to update clocks when option is selected
    Removed update every 1000ms

--- index.js
+++ index.js
@@ -7,17 +7,42 @@ let llnTimeElement = llnElement.querySelector(".time");
 let llnTime = moment().tz("Europe/Brussels");
 
 llnDateElement.innerHTML = llnTime.format("dddd, MMMM Do, YYYY");
-llnTimeElement.innerHTML = llnTime.format("H:M:s [<small>]A[</small>]");
+llnTimeElement.innerHTML = llnTime.format("hh:M:s [<small>]A[</small>]");
 
 //Bergen 
 
-let bergenElement = document.querySelector("#bergen");
-let bergenDateElement = bergenElement.querySelector(".date");
-let bergenTimeElement = bergenElement.querySelector(".time");
-let bergenTime = moment().tz("Europe/Oslo");
+//let bergenElement = document.querySelector("#bergen");
+//let bergenDateElement = bergenElement.querySelector(".date");
+//let bergenTimeElement = bergenElement.querySelector(".time");
+//let bergenTime = moment().tz("Europe/Oslo");
 
-bergenDateElement.innerHTML = bergenTime.format("dddd, MMMM Do, YYYY");
-bergenTimeElement.innerHTML = bergenTime.format("H:M:s [<small>]A[</small>]");
+//bergenDateElement.innerHTML = bergenTime.format("dddd, MMMM Do, YYYY");
+//bergenTimeElement.innerHTML = bergenTime.format("hh:M:s [<small>]A[</small>]");
 }
+
+function updateCity (event){
+    let cityTimezone = event.target.value;
+    let cityName = event.target.options[event.target.selectedIndex].text;
+    let cityTime = moment().tz(cityTimezone);
+    let citiesElement = document.querySelector("#cities");
+    
+    if (cityTimezone.length >0){
+        citiesElement.innerHTML =`<div class="city" id="lln">
+        <div>
+        <p>Time zone : ${cityTimezone}</p>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("dddd, MMMM Do, YYYY")}</div>
+        </div>
+
+        <div class="time">${cityTime.format("hh:M:ss [<small>]A[</small>]")}</div>
+    </div>
+    `;
+    }
+}
+
+
 upadteTime();
-setInterval(upadteTime,1000);
+//setInterval(upadteTime,1000);
+
+let selectedCityElement = document.querySelector("#selectedCity");
+selectedCityElement.addEventListener("change",updateCity);
